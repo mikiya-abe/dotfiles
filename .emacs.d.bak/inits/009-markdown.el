7479f3b58de12ab78b9a6ec9bf3944ea77b22ed9
Mikiya Abe <struuuuggle@gmail.com>
2019-09-09 19:24:47 +0900

    Deinit submodule(.emac.d)

--- .emacs.d.bak/inits/009-markdown.el
+++
@@ -1,7 +0,0 @@
-;;; 009-markdown.el --- Enable markdown-mode
-;;; Commentary:
-;;; Code:
-
-(autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
-
-;;; 009-markdown.el ends here
