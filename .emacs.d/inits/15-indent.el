0590758170ce5aa87f16939c58496a732f353e75
Mikiya Abe <struuuuggle@gmail.com>
2020-03-25 00:14:49 +0900

    Move tab configs to 15-indent.el

--- .emacs.d/inits/15-indent.el
+++ .emacs.d/inits/15-indent.el
@@ -1,6 +1,13 @@
 ;;; 15-indent.el --- Configs for indent-guildes
 ;;; Commentary:
 ;;; Code:
+
+;; タブ幅をスペース2つ分にする
+(setq-default tab-width 2)
+
+;; タブ文字ではなくスペースを使う
+(setq-default indent-tabs-mode nil)
+
 (use-package highlight-indent-guides
     :diminish
     :hook
